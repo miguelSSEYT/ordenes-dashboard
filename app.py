0e337637a9974e4d50a7c9463b0fbee851923873
miguelSSEYT <bs.miguelmtz@gmail.com>
2025-06-17 12:30:14 -0600

    app.py

--- app.py
+++ app.py
@@ -129,11 +129,12 @@ if crossref_file and mb52_file and coois_file and zco41_file:
         df['Net Inventory'] = df['Available after COOIS'] - df['Pln.Or Qty']
         df['Reason'] = df.apply(lambda row: (
         "✅ Can be produced — enough inventory available"
-        if row['Available after COOIS'] >= row['Pln.Or Qty']
-        else "❌ Cannot be produced — not enough inventory"
+            if row['Available after COOIS'] >= row['Pln.Or Qty']
+            else "❌ Cannot be produced — not enough inventory"
     ), axis=1)
     st.dataframe(df[['Sales Order', 'Custom Description', 'Pln.Or Qty',
                      'Available after COOIS', 'Net Inventory', 'Reason']])
+
     
     
     with st.expander("COOIS - Órdenes COMPLETAS que NO se pueden producir"):
